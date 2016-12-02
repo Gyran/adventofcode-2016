db3eb4f310de2574523765d2d9d873a9f8ad326f
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2016-12-02 23:55:45 +0100

    one solution

--- 02/index.js
+++ 02/index.js
@@ -1,50 +1,79 @@
 const fs = require('fs');
-const input = fs.readFileSync(`${ __dirname }/input`).toString();
+const RAW_INPUT = fs.readFileSync(`${ __dirname }/input`).toString();
+const INPUT_ROWS = RAW_INPUT.split('\n').slice(0, -1);
 
-const rows = input.split('\n').slice(0, -1);
-
-const part1 = (rows) => {
-
-};
-
-const PAD = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-const PAD_COLS = 3;
-const PAD_ROWS = 3;
-const MAP = {
+const INVALID_CHAR = ' ';
+const MOVEMENT_MAP = {
   U: [ 0, -1],
   D: [ 0,  1],
   L: [-1,  0],
   R: [ 1,  0],
 }
 
-const inInterval = (i, min, max) => {
-  return Math.min(Math.max(i, min), max);
+const PAD_1 = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+];
+const START_POS_1 = [1, 1];
+const PAD_2 = [
+  [' ', ' ', '1', ' ', ' '],
+  [' ', '2', '3', '4', ' '],
+  ['5', '6', '7', '8', '9'],
+  [' ', 'A', 'B', 'C', ' '],
+  [' ', ' ', 'D', ' ', ' '],
+];
+const START_POS_2 = [0, 2];
+
+const addPos = ([x1, y1], [x2, y2]) => {
+  return [x1 + x2, y1 + y2];
 }
 
-const addPos = (a, b) => {
-  const x = inInterval(a[0] + b[0], 0, PAD_COLS - 1);
-  const y = inInterval(a[1] + b[1], 0, PAD_ROWS - 1);
+const solve = (pad, startPos, rows) => {
+  const padCols = pad[0].length;
+  const padRows = pad.length;
 
-  return [x, y];
-}
+  const getPadChar = ([x, y]) => {
+    if (
+      x >= 0 && x < padCols &&
+      y >= 0 && y < padRows
+    ) {
+      return pad[y][x];
+    }
 
-const code = [];
-let currenPos = [1, 1];
-rows.forEach((row) => {
-  console.log(currenPos, 'doing row!', row);
+    return INVALID_CHAR;
+  };
 
-  for (let i = 0; i < row.length; i += 1) {
-    const direction = row[i];
-    currenPos = addPos(currenPos, MAP[row[i]]);
-    console.log('currenPos', direction, currenPos);
-  }
+  const tryToMove = (from, direction) => {
+    const newPos = addPos(from, MOVEMENT_MAP[direction]);
+    const char = getPadChar(newPos);
 
-  code.push(PAD[currenPos[1]][currenPos[0]]);
-  console.log(PAD[currenPos[1]][currenPos[0]]);
-});
+    if (char !== INVALID_CHAR) {
+      return newPos;
+    }
+    return from;
+  };
 
-console.log('The code is:', code.join(''));
+
+  let currenPos = startPos;
+  const code = [];
+
+  rows.forEach((row) => {
+    // console.log(currenPos, 'doing row!', row);
+
+    for (let i = 0; i < row.length; i += 1) {
+      const direction = row[i];
+      currenPos = tryToMove(currenPos, row[i]);
+    }
+
+    const char = getPadChar(currenPos);
+    code.push(char);
+  });
+
+  return code;
+};
+
+const solve1 = () => solve(PAD_1, START_POS_1, INPUT_ROWS);
+const solve2 = () => solve(PAD_2, START_POS_2, INPUT_ROWS);
+
+console.log('The code is:', solve2().join(''));
